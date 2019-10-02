9ec5d98cec7c2c2668c9248baa1b7d2e2f6d09ae
髙橋佑輔 <yusuke.takahasi@gmail.com>
2019-10-02 03:28:36 +0000

    php2

--- php_practice/practice.php
+++ php_practice/practice.php
@@ -44,3 +44,43 @@ $calendar2018 = [
 // 12月を表示する
 echo $calendar2018["December"];
 ?>
+
+<?php
+$name = "Yusuke";
+if ($name == "Yusuke") {
+    echo "私の名前はYusukeです";
+} else {
+    echo "あなたの名前ではありません" ;   
+}
+?>
+
+<?php
+$total = 0;
+echo $total;
+for($i = 0; $i <=10000; $i++) {
+    $total += $i;
+}
+    echo $total;
+?>
+
+<?php 
+$fruits = array("apple","banana","tomato","blueberry","pineapple");
+foreach($fruits as $fruit){
+    echo $fruit;
+    echo " ";
+}
+
+/* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 101;
+
+for($i = $start; $i < $end; $i++) {
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0) {
+    echo $i;
+  } else {
+      echo " ";
+  }
+}
